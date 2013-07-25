2723d1214723adeadd0c061fbd7b11d74505ef39
Daniel Doezema <dan@doezema.com>
2013-07-25 03:31:06 -0400

    Moving configuration settings to config.php

---
+++ config.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Author: Daniel Doezema
+ * Author URI: http://dan.doezema.com
+ * Version: 0.2 (Beta)
+ * Description: This script was developed to help ease migration of WordPress sites from one domain to another.
+ *
+ * Copyright (c) 2010, Daniel Doezema
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * The names of the contributors and/or copyright holder may not be
+ *     used to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL DANIEL DOEZEMA BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @copyright Copyright (c) 2010 Daniel Doezema. (http://dan.doezema.com)
+ * @license http://dan.doezema.com/licenses/new-bsd New BSD License
+ */
+
+/* == CONFIG ======================================================= */
+
+// Authentication Password
+// define('DDWPDC_PASSWORD', 'Replace-This-Password');
+define('DDWPDC_PASSWORD', '111');
+
+// Cookie: Name: Authentication
+define('DDWPDC_COOKIE_NAME_AUTH', 'DDWPDC_COOKIE_AUTH');
+
+// Cookie: Name: Expiration
+define('DDWPDC_COOKIE_NAME_EXPIRE', 'DDWPDC_COOKIE_EXPIRE');
+
+// Cookie: Timeout (Default: 5 minutes)
+define('DDWPDC_COOKIE_LIFETIME', 60 * 5);
